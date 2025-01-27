b956a060033648824fecc755235232f722eae78f
ilost01 <myaerophone@gmail.com>
2025-01-27 21:24:51 +0200

    Update AKolesnik_HW1.java

--- src/test/java/AKolesnik_HW1.java
+++ src/test/java/AKolesnik_HW1.java
@@ -22,43 +22,41 @@ public class AKolesnik_HW1 {
     public void cssLocators() {
         // Поиск элементов с использованием CSS-селекторов
 
-        // Найти заголовок PHONEBOOK
+        // заголовок PHONEBOOK
         WebElement title = wd.findElement(By.cssSelector("h1"));
-        assertNotNull(title, "Title element not found");
 
-        // Найти ссылку HOME
+
+        // HOME
         WebElement homeLink = wd.findElement(By.cssSelector("a[href='/home']"));
-        assertNotNull(homeLink, "Home link not found");
 
-        // Найти ссылку ABOUT
+
+        // ABOUT
         WebElement aboutLink = wd.findElement(By.cssSelector("a[href='/about']"));
-        assertNotNull(aboutLink, "About link not found");
 
-        // Найти кнопку LOGIN (используем класс и стиль для уникальности)
-        WebElement loginButton = wd.findElement(By.cssSelector("a[href='/login'].active"));
-        assertNotNull(loginButton, "Login button not found");
 
-        // Найти поле ввода Email
+        // кнопка LOGIN
+        WebElement buttonLogin = wd.findElement(By.cssSelector("button[name='login']"));
+
+
+        // кнопка REG
+        WebElement buttonReg = wd.findElement(By.cssSelector("button[name='registration']"));
+
+
+        // поле ввода Email
         WebElement emailInput = wd.findElement(By.cssSelector("input[name='email']"));
-        assertNotNull(emailInput, "Email input not found");
 
-        // Найти поле ввода Password
+
+        // поле ввода Password
         WebElement passwordInput = wd.findElement(By.cssSelector("input[name='password']"));
-        assertNotNull(passwordInput, "Password input not found");
 
-        // Найти кнопку Login внутри формы
-        WebElement loginSubmit = wd.findElement(By.cssSelector("button[name='login']"));
-        assertNotNull(loginSubmit, "Login submit button not found");
 
-        // Найти кнопку Registration внутри формы
-        WebElement registrationButton = wd.findElement(By.cssSelector("button[name='registration']"));
-        assertNotNull(registrationButton, "Registration button not found");
+
+
     }
 
     @AfterClass
     public void tearDown() {
-        if (wd != null) {
+
             wd.quit();
-        }
     }
 }
